c8dccdb95eeb504caf222cf179e625186217dbba
William Schroeder <wjschr18@stlawu.edu>
2020-01-29 14:07:32 -0500

    few values

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -8,12 +8,12 @@
   // 4. Print the smallest integer that is divisible by all the integers from 1 through 10.
   let flag = true;
   while (flag){
-    let y = 0;
+    let y = 1;
     if (y % 2===3 && y % 3 === 0 && y % 5 === 0 && y % 7 === 0){
       console.log(`4: ${y}`);
       flag = false;
     }else{
-      y++;
+      y += 1;
       console.log(y);
     }
   }
