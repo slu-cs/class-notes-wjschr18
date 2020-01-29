086c95be39b34f76913b2de2956a4cb476e44751
William Schroeder <wjschr18@stlawu.edu>
2020-01-29 13:37:17 -0500

    JavaScript syntax

---
+++ javascript/expressions.js
@@ -0,0 +1,55 @@
+  // Expression syntax in Javascript
+
+  // Booleans
+  true
+  false
+
+  //Logic
+  !false
+  true && false
+  true || false
+
+  //Numbers (all one type)
+  3.14
+
+  //Equality
+  3 === 3.0 // true
+  3 !== 3.14
+
+  //Ordering
+  3 > 2
+  3 >= 2
+  2 < 3
+  4 <= 4
+
+  //Strings
+  'hello'
+
+  //Concatenation
+  'a' + 'b' === 'ab'
+
+  //Arithmetic
+  3 + 2 === 5
+  3 - 2 === 1
+  3 * 2 === 6
+  3 / 2 === 1.5
+  3 % 2 === 1
+  3 ** 2 === 9
+
+  //Type coercion
+  '3' + 2 === '32'
+  '3' + true === '3true'
+  '3' - 2 === 1
+  3 + true === 4 // false === 0; true === 1
+
+  //Loose Equality
+  '3' == 3
+  true == 1
+  false == 0
+  false == ''
+  '' == 0
+
+  //Template Strings
+  '3+2 is ' + (3+2)
+  `3+2 is ${3+2}`
+  
---
+++ javascript/statements.js
@@ -0,0 +1,26 @@
+  //Statement syntax in Javascript
+
+  //Constants
+  const pi = 3.14;
+
+  //Printing
+  console.log(pi);
+
+  //For Loop
+  for(let x = 0; x < 3; x++){
+    console.log(x);
+  }
+
+  //While Loop
+  let x = 0;
+  while (x < 3) {
+    console.log(x);
+    x += 1;
+  }
+
+  //Conditional
+  if(x === 3){
+    console.log('x is now 3');
+  }else{
+    console.log(`x is now ${x}`);
+  }
