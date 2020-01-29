1c5de349a07d6125049dce335992ffcdef52d69f
William Schroeder <wjschr18@stlawu.edu>
2020-01-29 14:16:01 -0500

    syntax exercise

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -1,19 +1,20 @@
   // 1. Print the sum of the squares of the integers from 1 through 10.
   let x = 0;
   for(let i = 1; i <= 10; i++){
-    x = x + i ** 2;
+    x = x + (i ** 2);
   }
   console.log(`1: ${x}`);
 
-  // 4. Print the smallest integer that is divisible by all the integers from 1 through 10.
-  let flag = true;
-  while (flag){
-    let y = 1;
-    if (y % 2=== 0 && y % 3 === 0 && y % 5 === 0 && y % 7 === 0){
-      console.log(`4: ${y}`);
-      flag = false;
-    }else{
-      y += 1;
-      console.log(y);
+  //3. Print the number of integers from 1 through 100 that are divisible by 3 or 5 but not both.
+  let counter = 0;
+  for(let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 !== 0){
+      //console.log(`${i} is divisible by 3`);
+      counter++;
+    }
+    elseif (i % 5 === 0 && i % 3 !== 0){
+      //console.log(`${i} is divisible by 5`);
+      counter++;
     }
   }
+  console.log(`${counter} integers from 1-100 are divisible by 3 OR 5`);
