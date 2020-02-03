ff070adb8ca60f3fdcdf8d671f9b7ba7ebb97fc9
William Schroeder <wjschr18@stlawu.edu>
2020-02-03 13:07:15 -0500

    JavaScript object literals

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,5 +1,25 @@
 // Objects and array syntax
 
-// Object literal
+  // Object literal
   const point = {x:1 ,y: 2};
   console.log(point);
+
+  // accessing properties
+  console.log(point.x);
+
+  // Change properties
+  point.x = 3;
+
+  //Nested object literal
+  const circle = {
+    center: point,
+    radius: 4
+  };
+
+  //Access subproperties
+  console.log(circle.center.x);
+
+  // Undefined subproperties
+  console.log(circle.color);
+  circle.color = 'blue';
+  console.log(circle);
