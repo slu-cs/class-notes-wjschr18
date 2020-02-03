1ff4a1c2f160da49beff394280df02348057ab7e
William Schroeder <wjschr18@stlawu.edu>
2020-02-03 13:34:06 -0500

    JavaScript constructors and methods

--- javascript/functions.js
+++ javascript/functions.js
@@ -19,4 +19,33 @@
   print(9);
   print();
 
-  
+  // Object literal with a method
+  const rectangle = {
+    width: 10,
+    height: 20,
+    area: function(){
+      return this.width*this.height;
+    };
+  };
+
+  // Method call
+  console.log(rectangle.area());
+
+  // Constructor definition
+  const Rectangle = function(width, height){
+    this.width = width;
+    this.height = height;
+  };
+
+  // Shared method
+  Rectangle.prototype.area = function(){
+    return this.width*this.height;
+  };
+
+  // Constructing Objects
+  const small = new Rectangle(1,2);
+  const large = new Rectangle(10, 20);
+
+  // Method calls
+  console.log(small.area());
+  console.log(large.area());
