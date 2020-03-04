b75c5c9bf7a37bf0750751d668e13530f121a939
William Schroeder <wjschr18@stlawu.edu>
2020-03-04 13:04:59 -0500

    Express - Hello world

---
+++ express/hello/app.js
@@ -0,0 +1,14 @@
+  // Hello world web server
+  const express = require('express');
+
+  // Create the server
+  const app = express();
+
+  // Respond to one request
+  app.get('/', function(request, response){
+    respond.send('Hello Will');
+  });
+
+  // Start the server
+  app.listen(3000); // port = 3000
+  console.log('Server is ready');
