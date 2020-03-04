4dbae7c3b4eb2f9f66f20f352a3c27cb6ade4e08
William Schroeder <wjschr18@stlawu.edu>
2020-03-04 13:35:25 -0500

    express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -5,8 +5,29 @@
   const app = express();
 
   // Respond to one request
+  // cs-linuxlab-15.stlawu.edu:3000/
   app.get('/', function(request, response){
     response.send('Hello Will');
+  });
+
+  // cs-linuxlab-15.stlawu.edu:3000/foo
+  app.get('/foo', function(request, response){
+    response.send('Hello foo');
+  });
+
+  // cs-linuxlab-15.stlawu.edu:3000/foo/bar
+  app.get('/foo/bar', function(request, response){
+    response.send('Hello bar');
+  });
+
+  // cs-linuxlab-15.stlawu.edu:3000/zap/z (for any z)
+  app.get('/zap/:z', function(request, response){ // ':z' is like a parameter
+    response.send(`Hello ${request.params.z}`);
+  });
+
+  // cs-linuxlab-15.stlawu.edu:3000/zap?x=X&y=Y  // query string
+  app.get('/zap', function(request, response){
+    response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
   });
 
   // Start the server
