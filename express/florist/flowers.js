32afcb61bf4792eeeab04407a5509d06a495ca2c
William Schroeder <wjschr18@stlawu.edu>
2020-03-11 13:48:39 -0400

    REST API for AJAX example

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -3,4 +3,72 @@ const express = require('express');
 
 //create router
 const router = express.Router();
+
+// pretend database
+let flowers = [
+  {id: 'tulip', color: 'pink', season: 'spring'},
+  {id: 'rose', color: 'red', season: 'summer'}
+];
+
+// GET /flowers
+router.get('/', function(request, response){
+  response.send(flowers);
+});
+
+// GET /flowers/id
+
+router.get('/:id', function(request, response, next){
+  const flower = flowers.find(f => f.id === request.params.id) //lookup flower
+  if (!flower){ // cannot find flower
+    next(); // should lead to 404 in the end
+  } else {
+    response.send(flower);
+  }
+});
+
+// POST /flowers
+
+router.post('/', function(request, response){
+  const flower = request.body;
+  if (!flowers.id){
+    response.status(400).send('Missing ID');
+  }
+  if (flowers.find(f => f.id === flower.id)){
+    response.status(400).send('Duplicate'); // Bad request; can't create something that already exists
+  } else{
+    flowers.push(flower);
+    response.status(201).send(flowers); // .end() stops the sending of information
+  }
+});
+
+// DELETE /flowers/id
+
+router.delete('/:id', function(request, repsponse, next){
+  const flower = flowers.find(f => f.id === request.params.id)
+  if (!flower){
+    next();
+  } else {
+    flowers = flowers.filter(f => f.id !== flower.id);
+    response.status(200).send(flowers);
+  }
+});
+
+// PUT /flowers/id
+
+router.put('/:id', function(request, response, next){
+  const flower = flowers.find(f => f.id === request.params.id)
+  if (!flower){
+    next();
+  } else {
+    if (request.body.color){
+      flower.color = request.body.color;
+    }
+    if (request.body.season){
+      flower.season = request.body.season;
+    }
+    response.status(200).send(flowers);
+  }
+});
+
+
 module.exports = router;
